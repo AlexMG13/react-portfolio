ed067cba9f83891527009e3742ae6e2689c712f7
Alex <mendozaalex24@gmail.com>
2022-10-08 20:58:31 -0300

    solucione el tema del font family

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       <main className=' bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className=' text-xl font-burtons'>AlexDev</h1>
+            <h1 className=' text-2xl font-ComforterBrush'>AlexDev</h1>
             <ul className='flex items-center'>
               <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
               <li><a href="#" className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Mi CV</a></li>
